a94b1bd4969d443c0c1d6d51892eb04d02b16d75
neryportilla3312 <53507049+neryportilla3312@users.noreply.github.com>
2023-08-25 12:42:33 -0600

    Manipulando datos
    Se agrega la linea de codigo para contar los datos perdidos y el codigo para excluir datos

--- vector.R
+++ vector.R
@@ -7,6 +7,19 @@ fuma <- c('frecuente', 'nunca', 'a veces',
           NA, 'frecuente', NA, 'hola', 'nunca', 'hola', 
           'frecuente','nunca')
 
+table(fuma)
+
+# NA representar datos perdidos 
+# NULL representa la ausencia de datos 
+
+
+#contar los NA
+table(fuma, useNA = 'always')
+
+#excluir datos 
+table(fuma, exclude=c ('hola'))
+
+
 
 
 
