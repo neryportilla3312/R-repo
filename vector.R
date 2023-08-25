d4378a2bf40061bac8ef706bcfba4a92e3ee82aa
neryportilla3312 <53507049+neryportilla3312@users.noreply.github.com>
2023-08-25 12:24:13 -0600

    Vector
    se crea un vector con datos aleatorios

---
+++ vector.R
@@ -0,0 +1,13 @@
+#25 de agosto del 2023
+
+
+#se crea el vector 
+fuma <- c('frecuente', 'nunca', 'a veces', 
+          'a veces', 'a veces', 'nunca', 'frecuente',
+          NA, 'frecuente', NA, 'hola', 'nunca', 'hola', 
+          'frecuente','nunca')
+
+
+
+
+
